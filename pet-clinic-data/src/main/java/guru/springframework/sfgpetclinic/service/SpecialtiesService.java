2da8a47f4aea33be10cbf9ab509c361853ebf522
Madhu <smadhu.gowda@gmail.com>
2019-12-24 17:43:05 +0530

    Create Speciality Map Service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.service;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
