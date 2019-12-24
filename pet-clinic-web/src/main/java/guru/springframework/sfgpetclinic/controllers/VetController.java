be5dff803ec4407c9f4e7f3c178e8a304a19b049
Madhu <smadhu.gowda@gmail.com>
2019-12-24 18:24:46 +0530

    Menu Links broken fixed

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -15,7 +15,7 @@ public class VetController {
 		this.vetService = service;
 	}
 	
-	@RequestMapping({"/vets","/vets/index","/vets/index.html"})
+	@RequestMapping({"/vets","/vets/index","/vets/index.html","/vets.html"})
 	public String listVets(Model model) {
 		
 		model.addAttribute("vets" , vetService.findAll());
