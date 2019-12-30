d2af3ba4d39f6879d7e9b997072467d1ba3ee2a1
Madhu <smadhu.gowda@gmail.com>
2019-12-30 15:53:10 +0530

    Add SPring Data Repositories

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialityRepository  extends CrudRepository<Speciality, Long> {
+	
+}
